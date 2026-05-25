5fa7aede2261e738a5492d0cfc1690ac850dd031
danarseptiyanto <septiyandanar@gmail.com>
2026-05-25 21:28:44 +0700

    Update PWA icons and tweak Home layout
    Replace PWA icon assets (pwa-192x192.png, pwa-512x512.png) and adjust the Home component layout: change the wrapper div's className to apply mx-auto only at md breakpoint (mx-5 max-w-[1320px] md:mx-auto) so the container is centered on medium+ screens while retaining horizontal padding on smaller screens.

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,7 +2,7 @@ import { Head } from "@inertiajs/react";
 
 export default function Home() {
 	return (
-		<div className="mx-5 mx-auto max-w-[1320px] bg-red-600">
+		<div className="mx-5 max-w-[1320px] bg-red-600 md:mx-auto">
 			<div>Test</div>
 		</div>
 	);
