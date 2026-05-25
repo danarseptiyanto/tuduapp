e571b11c7365e7f914d85f48c7207ffb4f5695cc
danarseptiyanto <septiyandanar@gmail.com>
2026-05-25 20:52:46 +0700

    Update Home.jsx

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -3,7 +3,7 @@ import { Head } from "@inertiajs/react";
 export default function Home() {
 	return (
 		<div className="mx-5 mx-auto max-w-[1320px] bg-red-600">
-			<div>hehe</div>
+			<div>Test</div>
 		</div>
 	);
 }
