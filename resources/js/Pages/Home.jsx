047a3cc219e5f71cc0dd4a724c22d3004df4c9cf
danarseptiyanto <septiyandanar@gail.com>
2026-05-25 20:29:24 +0700

    Add .prettierignore and Home page component

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Head } from "@inertiajs/react";
+
+export default function Home() {
+	return (
+		<div className="mx-5 mx-auto max-w-[1320px] bg-red-600">
+			<div>hehe</div>
+		</div>
+	);
+}
